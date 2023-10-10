80f492efc1f74b8ad0c5be0948d99ab20faf8236
nareshm5796 <147257209+nareshm5796@users.noreply.github.com>
2023-10-10 20:04:41 +0530

    feild

--- javascript.js
+++ javascript.js
@@ -1,5 +1,48 @@
+// CONTACT SCRIPT--------
+
 const send_button = document.getElementsByClassName("btn")[2];
 console.log(send_button);
-send_button.onclick = function(){
-    alert("Submited Thank you..")
+
+const finput = document.getElementById("finput");
+const linput = document.getElementById("linput");
+const einput = document.getElementById("einput");
+// console.log(einput.value.lenght);
+
+send_button.onclick = function(){ 
+    var length = finput.value.length;
+    var first_name = parseInt(length);
+
+    var i= linput.value.length;
+    var last_name = parseInt(i);
+
+    var e = einput.value.lenght;
+    var email1 = parseInt(e);
+    
+    if (first_name == 0 ){
+        alert("Enter First Name");
+    }
+    else if(last_name == 0){
+        alert("Enter last Name");
+    }
+    
+    else{
+        alert("submited")
+    }
+        
+        
+       
 };
+
+
+
+
+
+
+
+// download SCRIPT--------
+const dwn_button = document.getElementsByClassName("btn")[1];
+console.log(dwn_button);
+dwn_button.onclick = function(){
+    alert("Resume not added...");
+};
+
